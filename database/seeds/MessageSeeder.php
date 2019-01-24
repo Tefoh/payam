bc7b78bf8403ec40e380d0fcd3dd9b0498eb23fb
tefo <tefo.ha27@gmail.com>
2019-01-24 13:22:03 +0330

    seeder for messages

---
+++ database/seeds/MessageSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Qasedak\Message;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 11; $i++) {
+            $user = factory(User::class)->create();
+            factory(Message::class, 10)->create(['author' => $user->id]);
+        }
+    }
+}
